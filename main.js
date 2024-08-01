ee4dbbaaa3940b599a143e25594faa6556c43602
Vihaan Mittal <86526176+Vihaan28@users.noreply.github.com>
2024-08-01 14:04:56 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+function home_page_rewards_btn(){
+    window.location="nutria-deliver.html"
+}
+
+function home(){
+    window.location="index.html"
+}
+
+function navbar_rewards(){
+    window.location="nutria-edu.html"
+}
+
+function navbar_giftcards(){
+    window.location="about-us.html"
+}
